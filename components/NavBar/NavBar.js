504814180bb7831880c5fc9d21f7f41131c737b6
Arian Sadeghi <arian.sadeghi03@outlook.com>
2022-12-26 13:58:17 -0500

    Created an initial nav bar

---
+++ components/NavBar/NavBar.js
@@ -0,0 +1,10 @@
+import React from "react"
+import styles from "./NavBar.module.css"
+
+export default function NavBar() {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>purposive</h1>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
+import NavBar from '../components/NavBar/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,9 +12,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-
-      </main>
+      <body>
+        <NavBar/>
+      </body>
     </>
   )
 }
